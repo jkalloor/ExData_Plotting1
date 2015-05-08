ca83adc570f03319ab6d53dccdc36634ff6c5597
jkalloor <jkalloor@yahoo.com>
2015-05-08 15:38:35 -0500

    plot3.R

--- plot3.R
+++ plot3.R
@@ -31,4 +31,5 @@ with(epc4,plot(DateTime,Sub_metering_1,type="l",col="black",ylab="Energy sub met
 with(epc4,lines(DateTime,Sub_metering_2,type="l",col="red"  ))
 with(epc4,lines(DateTime,Sub_metering_3,type="l",col="blue"))
 legend("topright",lty=1,col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.off()
+title("Plot3",outer=TRUE,adj=0,line= -1)
+dev.off() 
